e3889f669dd25f575b01c206c55e008faae529fa
Aditya Tiwari <thegeekyboy.in@gmail.com>
2025-07-13 00:33:29 +0530

    Update App.jsx to resolve vercel header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Header } from "./components/header";
+import { Header } from "./components/Header";
 import HeroSec from "./components/HeroSec";
 import StackingCards from "./components/StackedCards";
 
